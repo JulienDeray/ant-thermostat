17c4b8ef3f6c7a16e96df902489c51264c69be7d
Julien Déray <julien@deray.fr>
2016-01-16 17:42:09 +0000

    Improves mocked temperature

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,11 @@ var express = require('express');
 var router = express.Router();
 
 var TemperatureService = require('../lib/src/services/temperature.service.js');
+var temperatureService = new TemperatureService(27);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { temperature: TemperatureService.getTemperature() });
+  res.render('index', { temperature: temperatureService.getTemperature() });
 });
 
 module.exports = router;

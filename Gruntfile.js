c5a9e81a8ad1dde5b9dafbed5b13fde20e7647e4
Julien Déray <julien@deray.fr>
2016-01-16 13:29:29 +0000

    Sets up gower and grunt (watch, bowerconcat, concatcss)

---
+++ Gruntfile.js
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2015 PDX Technology, All rights reserved.
+ *
+ * Unpublished copyright. All rights reserved. This material contains
+ * proprietary information that shall be used or copied only with
+ * PDX Technology, except with written permission of PDX Technology.
+ *
+ */
+
+module.exports = function(grunt) {
+
+grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    bower_concat: {
+        all: {
+            dest: 'public/dist/bower.js'
+        }
+    },
+    bower: {
+        install: {
+            options: {
+                install: true,
+                copy: false,
+                targetDir: '.libs',
+                cleanTargetDir: true
+            }
+        }
+    },
+    watch: {
+        options: {
+          livereload: true
+        },
+        dev: {
+            files: [ 'Gruntfile.js', 'app.js', 'lib/**/*.js', 'public/**', 'views/**/*.ejs' ],
+            tasks: [ 'express:dev' ],
+            options: {
+                atBegin: true,
+                spawn: false
+            }
+        }
+    },
+    concat_css: {
+        options: {},
+        all: {
+            src: [
+                "bower_components/bootstrap/dist/css/bootstrap.min.css"
+            ],
+            dest: "public/dist/bower.css"
+        }
+    },
+    express: {
+      options: {
+      },
+      dev: {
+        options: {
+          script: 'bin/www'
+        }
+      }
+    }
+});
+
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-bower-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-concat-css');
+    grunt.loadNpmTasks('grunt-bowercopy');
+    grunt.loadNpmTasks('grunt-express-server');
+
+    grunt.registerTask('default', ['bower', 'bower_concat', 'concat_css', 'express:dev']);
+    grunt.registerTask('build', ['bower', 'bower_concat', 'concat_css']);
+    grunt.registerTask('dev', ['build', 'watch:dev']);
+};

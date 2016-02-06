90ad1baebaf7b6784474c7f18f6e9b3978dd4ab5
Julien Déray <julien@deray.fr>
2016-02-06 12:20:34 +0000

    tests the regulation system

--- test/thermostat.spec.js
+++ test/thermostat.spec.js
@@ -8,24 +8,82 @@ let sinon = require('sinon'),
 let ThermostatsService = rewire('../lib/src/services/thermostats.service.js');
 
 describe('ThermostatsService', function() {
-  describe('starting up', function () {
-    it('should heat up and stabilize at the given temperature', function () {
-      const initialTemperature = 23;
+  let start,
+    stop,
+    thermostatsService;
 
-      const start = sinon.spy();
-      const stop = sinon.spy();
+  beforeEach(function() {
+    start = sinon.spy();
+    stop = sinon.spy();
+    const recordTemperature = sinon.spy();
 
-      ThermostatsService.__set__({
-        heaterService: {
-          start: start,
-          stop: stop
-        }
-      });
+    ThermostatsService.__set__({
+      heaterService: {
+        start: start,
+        stop: stop
+      },
+      monitorService: {
+        recordTemperature: recordTemperature
+      }
+    });
 
-      let thermostatsService = new ThermostatsService();
-      thermostatsService.regulate(start, start);
+    thermostatsService = new ThermostatsService();
+  });
 
-      start.calledOnce.should.equal(true, 'startHeater not called');
+  describe('bellow the threshold - going up', function() {
+    it('should start heating', function() {
+      thermostatsService.regulate(23, 23);
+      start.called.should.equal(true, "heater didn't start");
+    });
+
+    it('should continue heating', function() {
+      thermostatsService.regulate(24, 23);
+      start.called.should.equal(true, 'heater stopped');
+    });
+
+    it('should stop heating', function() {
+      thermostatsService.regulate(24, 22);
+      stop.called.should.equal(true, "heater didn't stop");
+    });
+  });
+
+  describe('bellow the threshold - going down', function() {
+    it('should restart the heater', function() {
+      thermostatsService.regulate(24, 25);
+      start.calledOnce.should.equal(true, "heater didn't start again");
+    });
+  });
+
+  describe('exceeding the threshold - going up', function() {
+    it('should let the heater off', function() {
+      thermostatsService.regulate(27, 25);
+      stop.called.should.equal(true, "heater didn't stop");
+    });
+  });
+
+  describe('exceeding the threshold - going down', function() {
+    it('should startthe heater', function() {
+      thermostatsService.regulate(25, 27);
+      start.called.should.equal(true, "heater didn't start");
+    });
+  });
+
+  describe('greater than the threshold - going up', function() {
+    it('should stop the heater', function() {
+      thermostatsService.regulate(28, 27);
+      stop.calledOnce.should.equal(true, "heater didn't stop");
+    });
+  });
+
+  describe('greater than the threshold - going down', function() {
+    it('should stop the heater', function() {
+      thermostatsService.regulate(27, 28);
+      stop.calledOnce.should.equal(true, "heater didn't stop");
+    });
+
+    it('should start the heater', function() {
+      thermostatsService.regulate(26, 27);
+      start.calledOnce.should.equal(true, "heater didn't start");
     });
   });
 });

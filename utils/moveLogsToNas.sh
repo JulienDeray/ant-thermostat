8d93ff6972396f37679869943300884753c0785b
Julien Déray <julien@deray.fr>
2016-02-28 12:11:22 +0000

    refactors script

--- utils/moveLogsToNas.sh
+++ utils/moveLogsToNas.sh
@@ -1,10 +1,26 @@
-#! /bin/zsh
+#!/bin/sh
 
-ssh root@ananas.local 'ls /volume1/homes/julienderay/ant-logs' > ananas-logs.tmp
-ls ant-logs > local-logs.tmp
+cd /home/pi
+eval `ssh-agent -s`
+SSH_AUTH_SOCK=/tmp/ssh-J9m35ZYHxXwg/agent.24232
 
-diff ananas-logs.tmp local-logs.tmp | grep '>' | awk '{print $2}' > diff.tmp
+SERVER='root@ananas.local'
+SERVER_LOG_DIR='/volume1/homes/julienderay/ant-logs'
+LOCAL_LOG_DIR='/home/pi/ant-logs'
 
-while read p; do
-  scp ant-logs/$p root@ananas.local:/volume1/homes/julienderay/ant-logs
-done < diff.tmp
+TMP_SERVER_LOGS_LIST='/home/pi/ananas-logs.tmp'
+TMP_LOCAL_LOGS_LIST='/home/pi/local-logs.tmp'
+DIFF_FILES_LIST='/home/pi/diff.tmp'
+
+ssh $SERVER "ls $SERVER_LOG_DIR" > $TMP_SERVER_LOGS_LIST
+ls $LOCAL_LOG_DIR > $TMP_LOCAL_LOGS_LIST
+
+diff $TMP_SERVER_LOGS_LIST $TMP_LOCAL_LOGS_LIST | grep '>' | awk '{print $2}' > $DIFF_FILES_LIST
+
+while read logFile; do
+  scp $LOCAL_LOG_DIR/$logFile $SERVER:$SERVER_LOG_DIR
+done < $DIFF_FILES_LIST
+
+rm $TMP_LOCAL_LOGS_LIST $TMP_SERVER_LOGS_LIST $DIFF_FILES_LIST
+
+kill $SSH_AGENT_PID
